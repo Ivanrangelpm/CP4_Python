3207d47ef0ad84718edf71dae67fe93beb95b309
Ivan-fakeejs <126684471+Ivanrangelpm@users.noreply.github.com>
2023-08-31 13:02:26 -0300

    Add files via upload

---
+++ cp4/CheckpointJogoDaVelha.py
@@ -0,0 +1,173 @@
+import random
+
+def ImprimeMenuPrincipal():
+    print("Bem-vindo ao Jogo da Velha!")
+    print("Escolha uma opção:")
+    print("1. Jogar contra outro jogador")
+    print("2. Jogar contra a máquina")
+    escolha = input()
+    
+    if escolha == "1":
+        for rodar in range(3):
+            JogoDaVelha()
+    elif escolha == "2":
+        for rodar in range(3):
+            JogoDaVelhaSistema()
+    else:
+        print("Opção inválida. Digite novamente.")
+        ImprimeMenuPrincipal()
+
+def JogoDaVelha():
+
+    tabuleiro = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    vitoria = False
+
+    def ImprimirTabuleiro():
+        print("\n")
+        print('', tabuleiro[0], "!", tabuleiro[1], "!", tabuleiro[2])
+        print("---!---!---")
+        print('', tabuleiro[3], "!", tabuleiro[4], "!", tabuleiro[5])
+        print("---!---!---")
+        print('', tabuleiro[6], "!", tabuleiro[7], "!", tabuleiro[8])
+        print("\n")
+    def ChecaVitoria(jogador):
+        for a in range(0, 7, 3):
+            if tabuleiro[a] == tabuleiro[a + 1] == tabuleiro[a + 2] == jogador:
+                return True
+        for a in range(3):
+            if tabuleiro[a] == tabuleiro[a + 3] == tabuleiro[a + 6] == jogador:
+                return True
+        if tabuleiro[0] == tabuleiro[4] == tabuleiro[8] == jogador:
+            return True
+        if tabuleiro[2] == tabuleiro[4] == tabuleiro[6] == jogador:
+            return True
+        return False
+
+    def PegarNumero():
+        while True:
+            numero = input()
+            try:
+                numero = int(numero)
+                if numero in range(1, 10):
+                    return numero
+                else:
+                    print("\nNúmero não está no tabuleiro.")
+            except ValueError:
+                print("\nIsso não é um número. Tente novamente.")
+                continue
+
+
+    def Turno(jogador):
+        espaco_colocado = PegarNumero() - 1
+        if tabuleiro[espaco_colocado] == "X" or tabuleiro[espaco_colocado] == "O":
+            print("\nEspaço já ocupado. Tente colocar em outro.")
+            Turno(jogador)
+        else:
+            tabuleiro[espaco_colocado] = jogador
+
+    jogadas = 0
+    while not vitoria:
+        ImprimirTabuleiro()
+        vitoria = ChecaVitoria("O")
+        if vitoria:
+            print("Jogador O venceu!")
+            break
+
+        if jogadas == 9:
+            print("O jogo acabou em empate.")
+            break
+
+        print("Jogador X, escolha um espaço.")
+        Turno("X")
+        jogadas += 1
+
+        ImprimirTabuleiro()
+        vitoria = ChecaVitoria("X")
+        if vitoria:
+            print("Jogador X venceu!")
+            break
+
+        print("Jogador O, escolha um espaço.")
+        Turno("O")
+        jogadas += 1    
+
+def JogoDaVelhaSistema():
+    tabuleiro = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    vitoria = False
+
+    def ImprimirTabuleiro():
+        print("\n")
+        print('', tabuleiro[0], "!", tabuleiro[1], "!", tabuleiro[2])
+        print("---!---!---")
+        print('', tabuleiro[3], "!", tabuleiro[4], "!", tabuleiro[5])
+        print("---!---!---")
+        print('', tabuleiro[6], "!", tabuleiro[7], "!", tabuleiro[8])
+        print("\n")
+
+    def ChecaVitoria(jogador):
+        for a in range(0, 7, 3):
+            if tabuleiro[a] == tabuleiro[a + 1] == tabuleiro[a + 2] == jogador:
+                return True
+        for a in range(3):
+            if tabuleiro[a] == tabuleiro[a + 3] == tabuleiro[a + 6] == jogador:
+                return True
+        if tabuleiro[0] == tabuleiro[4] == tabuleiro[8] == jogador:
+            return True
+        if tabuleiro[2] == tabuleiro[4] == tabuleiro[6] == jogador:
+            return True
+        return False
+
+    def PegarNumero():
+        while True:
+            numero = input()
+            try:
+                numero = int(numero)
+                if numero in range(1, 10):
+                    return numero
+                else:
+                    print("\nNúmero não está no tabuleiro.")
+            except ValueError:
+                print("\nIsso não é um número. Tente novamente.")
+                continue
+
+    def Turno(jogador):
+        espaco_colocado = PegarNumero() - 1
+        if tabuleiro[espaco_colocado] == "X" or tabuleiro[espaco_colocado] == "O":
+            print("\nEspaço já ocupado. Tente colocar em outro.")
+            Turno(jogador)
+        else:
+            tabuleiro[espaco_colocado] = jogador
+           
+
+    jogadas = 0
+    while not vitoria:
+        ImprimirTabuleiro()
+        vitoria = ChecaVitoria("O")
+        if vitoria:
+            print("Jogador O venceu!")
+            break
+
+        if jogadas == 9:
+            print("O jogo acabou em empate.")
+            break
+
+        print("Jogador X, escolha um espaço.")
+        Turno("X")
+        jogadas += 1
+
+        ImprimirTabuleiro()
+        vitoria = ChecaVitoria("X")
+        if vitoria:
+            print("Jogador X venceu!")
+            break
+
+        print("Vez do sistema escolher um espaço.")
+        TurnoSistema()
+        jogadas += 1
+
+    def TurnoSistema():
+        espacos_vazios = [i for i, valor in enumerate(tabuleiro) if valor != "X" and valor != "O"]
+        jogada_sistema = random.choice(espacos_vazios)
+        tabuleiro[jogada_sistema] = "O"
+
+ImprimeMenuPrincipal()
